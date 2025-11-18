c91c24bbd28f105240a3432cb10d0b691b00ca9d
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2025-11-18 15:47:33 +0200

    CalculatorPageViewModel

---
+++ SciCalc/SciCalc/ViewModels/CalculatorPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+
+namespace SciCalc.ViewModels
+{
+    [INotifyPropertyChanged]
+    internal partial class CalculatorPageViewModel //: ObservableObject
+    {
+        [ObservableProperty]
+        private string inputText = string.Empty;
+
+        [ObservableProperty]
+        private string calculatedResult = "0";
+
+        private bool isSciOpWaiting = false;
+
+    }
+}
