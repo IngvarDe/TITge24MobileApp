33591e284f2b510bf339156f2eb9d6c4183eb16d
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2025-12-02 15:47:20 +0200

    PlanetDetailsPage

---
+++ MauiPlanets/MauiPlanets/Views/PlanetDetailsPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace MauiPlanets.Views;
+
+public partial class PlanetDetailsPage : ContentPage
+{
+	public PlanetDetailsPage()
+	{
+		InitializeComponent();
+	}
+
+    private void BackButton_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
