c6053a33fff61eb0908c5dd08cd3bff975ced0cf
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2025-12-03 12:10:07 +0200

    Products

---
+++ MauiCRUD/MauiCRUD/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MauiCRUD.Data;
+using MauiCRUD.Models;
+using System.Collections.ObjectModel;
+
+
+namespace MauiCRUD.ViewModels
+{
+    public partial class ProductsViewModel : ObservableObject
+    {
+        private readonly DatabaseContext _context;
+
+        public ProductsViewModel(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [ObservableProperty]
+        private ObservableCollection<Product> _products = new();
+
+        [ObservableProperty]
+        private Product _operatingProduct = new();
+
+        [ObservableProperty]
+        private bool _isBusy;
+
+        [ObservableProperty]
+        private string _busyText;
+    }
+}
