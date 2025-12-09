ad6fc3de21552044c15cbfb70af49172e04202be
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2025-12-09 15:50:12 +0200

    mainPageClass

--- MauiCRUD/MauiCRUD/MainPage.xaml.cs
+++ MauiCRUD/MauiCRUD/MainPage.xaml.cs
@@ -1,13 +1,25 @@
-﻿namespace MauiCRUD
+﻿using MauiCRUD.ViewModels;
+
+namespace MauiCRUD
 {
     public partial class MainPage : ContentPage
     {
+        private readonly ProductsViewModel _productsViewModel;
 
-        public MainPage()
+        public MainPage
+            (
+                ProductsViewModel productsViewModel
+            )
         {
             InitializeComponent();
+            BindingContext = productsViewModel;
+            _productsViewModel = productsViewModel;
         }
 
-
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            await _productsViewModel.LoadProductAsync();
+        }
     }
 }
